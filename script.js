51bbfd6684693628fa791080a4e4e2b8dddf120d
eric5605 <eah7279@live.com>
2020-09-28 16:16:52 -0400

    added json packages, and basic js and html

---
+++ script.js
@@ -0,0 +1,45 @@
+class CurrencyConverter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rate: 0.89,
+      usd: 1,
+      euro: 1 * 0.89,
+    };
+  }
+
+  toUsd(amount, rate) {
+    return amount * (1 / rate);
+  }
+
+  toEuro(amount, rate) {
+    return amount * rate;
+  }
+
+  render() {
+    const { rate, usd, euro } = this.state;
+
+    return (
+      <div className="container">
+        <div className="text-center p-3 mb-2">
+          <h2 className="mb-2">Currency Converter</h2>
+          <h4>USD 1 : {rate} EURO</h4>
+        </div>
+        <div className="row text-center">
+          <div className="col-12">
+            <span className="mr-1">USD</span>
+            <input value={usd} onChange={this.handleUsdChange} type="number" />
+            <span className="mx-3">=</span>
+            <input value={euro} onChange={this.handleEuroChange} type="number" />
+            <span className="ml-1">EURO</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <CurrencyConverter />,
+  document.getElementById('root')
+);
